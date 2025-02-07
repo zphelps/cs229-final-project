63fb3a33d7b3cfa89bb3b95bf66caf2e8c02ff82
zphelps <zach@zachphelps.com>
2025-02-06 19:43:00 -0800

    dataset builder

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 
+import pandas as pd
 
 
-
+def get_races(year: int):
+    races = pd.read_csv("data/races.csv")
+    return races[races["year"].isin([year])]
 
 
 def main():
-    print("Hello, World!")
+    results = pd.read_csv("data/results.csv")
+    races = pd.read_csv("data/races.csv")
+
+    
+    print(results.head())
+    print(races.head())
+
+    results = results.merge(races, on="raceId", how="inner")
+
+    results = results.query("year == 2024")
+    print(results.head())
 
 
 if __name__ == "__main__":
